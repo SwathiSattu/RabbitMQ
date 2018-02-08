362e0498066e8cf0216d17f5009a0e54679adca3
vasu34k123 <vasu34k@gmail.com>
2018-02-08 17:24:40 +0530

    Create EmitLog.java

---
+++ EmitLog.java
@@ -0,0 +1,48 @@
+mport java.io.IOException;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class EmitLog {
+
+	private static final String EXCHANGE_NAME = "exchange1";
+
+	public static void main(String[] argv) throws java.io.IOException {
+		String message=null;
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+	
+for(int a=2;a<99;a++)
+{
+	message = getMessage(argv)+a;		
+	channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+			
+}
+		System.out.println(" [x] Sent '" + message + "'");
+
+		channel.close();
+		connection.close();
+	}
+
+	private static String getMessage(String[] strings) {
+		if (strings.length < 1)
+			return "info: Hello World!";
+		return joinStrings(strings, " ");
+	}
+
+	private static String joinStrings(String[] strings, String delimiter) {
+		int length = strings.length;
+		if (length == 0)
+			return "";
+		StringBuilder words = new StringBuilder(strings[0]);
+		for (int i = 1; i < length; i++) {
+			words.append(delimiter).append(strings[i]);
+		}
+		return words.toString();
+	}
+}
